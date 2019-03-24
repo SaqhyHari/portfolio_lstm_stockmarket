2cc3143eae416328c643c2668023857ade6ce6c5
Saqhy Hari <48218182+SaqhyHari@users.noreply.github.com>
2019-03-24 14:48:47 +0530

    Add files via upload

--- openanaconda.php
+++ openanaconda.php
@@ -1,6 +1,17 @@
-<htlm>
+<html>
+<head>
+
+<script type="text/javascript">
+  window.onclick = function(){
+   window.location.href = 'dash.php';};
+</script>
+
+</head>
 
 <body>
+
+<img src="wall.gif" width="100%" height="100%">
+
 <?php
 execInBackground('start cmd.exe @cmd /K "C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3 && python C:\xampp\htdocs\Final\portfolio.py"');
 function execInBackground($cmd) { 
@@ -12,7 +23,6 @@ function execInBackground($cmd) {
         exec($cmd . " > /dev/null &");   
     } 
 }
-//header("Location: dash.php");
 ?>
 </body>
-</htlm>
+</html>
